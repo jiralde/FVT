e0c2cafddcf4c032de52668638123b68202559d9
GiovanniPeirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-05-07 11:53:43 -0300

    asd

---
+++ Hospital La Falda/Scripts/Novevdades.js
@@ -0,0 +1,12 @@
+let Largo = "550px";
+
+let noticia = document.getElementById("l");
+
+noticia.style.height = Largo;
+
+function Alargar(){
+    let noticia = document.getElementById("l");
+    let Largo = "550px";
+    Largo = Largo * Largo;
+    noticia.style.height = Largo;
+}
