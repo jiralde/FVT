facb0251e8b015c47e25861ec755f7fa90edd1ab
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-05-27 23:20:09 -0300

    b

--- Hospital La Falda/Scripts/consultas.js
+++ Hospital La Falda/Scripts/consultas.js
@@ -1,10 +1,22 @@
-function chat(){
-    const Result = document.getElementById("chat");
-    Result.innerText = `
-    <div class="chat-text">
+/*const container = document.querySelector("body");
+
+const img = document.createElement("img");
+img.src = "https://lenguajejs.com/assets/logo.svg";
+img.alt = "Logo Javascript";
+
+container.appendChild(img);
+
+*/
+function Desplegable() 
+{
+    const ContenedorDIV = document.querySelector("C_DIV_1");
+    ContenedorDIV = document.createElement(`
+    <div class="a">
+        a
     </div>
-    <div class="input-chat" >
-        <input class="input" type="text" id="pregunta">
-        <button class="button-Enviar" id="Enviar">Enviar</button>
-    </div>"`;
+    `);
+
+
+
+
 }
