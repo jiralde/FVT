daf2c0c956f45e6860ad97fee6511d965c90d6ac
GiovanniPeirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-05-28 13:56:16 -0300

    d

--- Hospital La Falda/Scripts/consultas.js
+++ Hospital La Falda/Scripts/consultas.js
@@ -1,47 +1,36 @@
-var bool = new Boolean(true);
+var bool = Boolean(true);
+let div = `
+<div class="a">
+    <p>Para sacar un turno deves ir a la seccion de turnos 
+        que aparese en la parte superior de la pagina
+    </p>
+    <br>
+    <img src="img/header.png" alt="headerIMG" width="800px">
+    <br>
+    <p>Luego deves rellenar la informacion</p>
+    <br>
+    <img src="img/Datos.png" alt="DatosIMG" width="800px">
+    <br>
+    <p>Y  por ultimo enviar</p>
+    <br>
+    <img src="img/Enviar.png" alt="enviarIMG" width="800px">
+</div>
+`;
+
+
 function Desplegable() 
 {
     if (bool == true){
-        document.getElementById("aa").innerHTML += 
-        `
-        <div class="a">
-            <p>Para sacar un turno deves ir a la seccion de turnos 
-                que aparese en la parte superior de la pajina
-            </p>
-            <br>
-            <img src="img/header.png" alt="headerIMG" width="800px">
-            <br>
-            <p>Luego deves rellenar la informacion</p>
-            <br>
-            <img src="img/Datos.png" alt="DatosIMG" width="800px">
-            <br>
-            <p>Y  por ultimo enviar</p>
-            <br>
-            <img src="img/Enviar.png" alt="enviarIMG" width="800px">
-        </div>
-        `
-        bool = new Boolean(false);
+        document.getElementById("aa").innerHTML += div ;
+        bool = Boolean(false);
+        if (isNaN(bool || div)){
+            console.log("IsNAN");
+        }
     }else{
-        document.getElementById("aa").innerHTML -= 
-        `
-        <div class="a">
-            <p>Para sacar un turno deves ir a la seccion de turnos 
-                que aparese en la parte superior de la pajina
-            </p>
-            <br>
-            <img src="img/header.png" alt="headerIMG" width="800px">
-            <br>
-            <p>Luego deves rellenar la informacion</p>
-            <br>
-            <img src="img/Datos.png" alt="DatosIMG" width="800px">
-            <br>
-            <p>Y  por ultimo enviar</p>
-            <br>
-            <img src="img/Enviar.png" alt="enviarIMG" width="800px">
-        </div>
-        `
-        bool = new Boolean(true);
-    }
-   
-    
+        document.getElementById("aa").innerHTML -= div;
+        if (isNaN(bool || div)){
+            document.querySelector("NaN");
+        }
+        bool = Boolean(true);
+    }   
 }
