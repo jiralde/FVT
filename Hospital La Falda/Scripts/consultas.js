556cc479e68b8f99f3751da266a429490151ad95
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-05-28 12:03:37 -0300

    :+1:

--- Hospital La Falda/Scripts/consultas.js
+++ Hospital La Falda/Scripts/consultas.js
@@ -1,22 +1,47 @@
-/*const container = document.querySelector("body");
-
-const img = document.createElement("img");
-img.src = "https://lenguajejs.com/assets/logo.svg";
-img.alt = "Logo Javascript";
-
-container.appendChild(img);
-
-*/
+var bool = new Boolean(true);
 function Desplegable() 
 {
-    const ContenedorDIV = document.querySelector("C_DIV_1");
-    ContenedorDIV = document.createElement(`
-    <div class="a">
-        a
-    </div>
-    `);
-
-
-
-
+    if (bool == true){
+        document.getElementById("aa").innerHTML += 
+        `
+        <div class="a">
+            <p>Para sacar un turno deves ir a la seccion de turnos 
+                que aparese en la parte superior de la pajina
+            </p>
+            <br>
+            <img src="img/header.png" alt="headerIMG" width="800px">
+            <br>
+            <p>Luego deves rellenar la informacion</p>
+            <br>
+            <img src="img/Datos.png" alt="DatosIMG" width="800px">
+            <br>
+            <p>Y  por ultimo enviar</p>
+            <br>
+            <img src="img/Enviar.png" alt="enviarIMG" width="800px">
+        </div>
+        `
+        bool = new Boolean(false);
+    }else{
+        document.getElementById("aa").innerHTML -= 
+        `
+        <div class="a">
+            <p>Para sacar un turno deves ir a la seccion de turnos 
+                que aparese en la parte superior de la pajina
+            </p>
+            <br>
+            <img src="img/header.png" alt="headerIMG" width="800px">
+            <br>
+            <p>Luego deves rellenar la informacion</p>
+            <br>
+            <img src="img/Datos.png" alt="DatosIMG" width="800px">
+            <br>
+            <p>Y  por ultimo enviar</p>
+            <br>
+            <img src="img/Enviar.png" alt="enviarIMG" width="800px">
+        </div>
+        `
+        bool = new Boolean(true);
+    }
+   
+    
 }
